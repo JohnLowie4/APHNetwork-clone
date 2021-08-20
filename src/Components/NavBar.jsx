175c2241c462da8a3aacbd27f1009ba0893697d3
John <jzlowie@gmail.com>
2021-08-19 21:10:27 -0600

    bare bones nav is complete, needs styling

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from '../img/aph-logo.png';
+import './Navbar.css';
 
-function NavBar() {
-  return (
-    <header className="Nav-bar">
-      <img src={logo} className="Nav-logo"/>
-      <a>APH Networks</a>
-      <a>Since Forever</a>
-    </header>
-  );
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <div className="navbar-logo-name">
+          <img src={logo} className="navbar-logo" alt="APH logo"/>
+          <div className="navbar-name">
+            <h1>APH Networks</h1>
+            <h3>Since Forever</h3>
+          </div>
+        </div>
+        <div className="navbar-routes">
+          <ul className="routes">
+            <li>Home</li>
+            <li>Categories</li>
+            <li>Upcoming</li>
+            <li>About</li>
+          </ul>
+        </div>
+
+      </nav>
+    )
+  }
 }
 
-export default NavBar;
+export default Navbar;
