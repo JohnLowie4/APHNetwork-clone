877cfecbdb25723233b1fb01d1fe0226fee80a04
John <jzlowie@gmail.com>
2021-08-27 14:34:02 -0600

    Refactoring navbar to include routes/building nav from scratch again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import "./App.css";
-import Navbar from "./Components/Navbar";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Topics from "./Components/Topics";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
